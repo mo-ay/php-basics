dc160dca746c293e7cdd565153113a91c0c6bf60
mo-ay <mostafaaanezan@gmail.com>
2020-10-05 20:53:16 +0300

    Completed step4_string-concatenation

---
+++ step4_string-concatenation.php
@@ -0,0 +1,7 @@
+<?php
+$name = "mostafa" ;
+$position = " student";
+$github_url = " https://github.com/mo-ay/"; 
+echo " hello, I'm ".$name." I'm ".$position>" please check my github link ".$github_url;
+echo "\n hello, I'm $name I'm $position please check my github link $github_url";
+echo 'hello, I"m '.$name.' I"m '.$position.' please check my github link '.$github_url;
