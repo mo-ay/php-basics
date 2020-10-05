8e1f24fe85039b43c52615207d367db40afe56a1
mo-ay <mostafaaanezan@gmail.com>
2020-10-05 20:36:44 +0300

    Completed step3_variables01

---
+++ step3_variables01.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Mostafa";
+$email = "mostafaaanezan@gmail.com";
+echo 'my name is ' .$name .' and my email is '.$email;
+echo "\nmy name is $name and my email is $email \n";
+$height = 182;
+$width = 71;
+$area = $height * $width ;
+print($area);
+?>
